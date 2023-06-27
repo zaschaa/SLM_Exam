2a5382712f51d27a44cbdff7901e05c234b2dcc6
zaschaa <wi22b025@technikum-wien.at>
2023-06-27 22:38:03 +0200

    initial commit

---
+++ src/test/java/com/example/slm_exam/SlmExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slm_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
